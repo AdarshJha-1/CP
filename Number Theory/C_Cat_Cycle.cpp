53ff2c5c1f5f52e245efb04be2c44ecafaac801c
Adarsh Jha <adarsh2005j@gmail.com>
2026-05-26 21:40:20 +0530

    Number Theory: F* the GCD

---
+++ Number Theory/C_Cat_Cycle.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+/*
+Initially I understood by visualizing that I have to somehow find the number of collisions.
+
+A move index-- and B move index++, so I thought collision is the main thing here.
+
+Then after dry runs I got the formula that for a given n, collision happens at every:
+(n - 1) / 2
+
+moves.
+
+Then I thought I should also calculate how many total collisions can happen given I can move only k times.
+Then I realized I actually have only k - 1 moves because move 1 is just the initial value position and no movement happens there.
+
+So I thought:
+
+collisions = (k - 1) / collision_interval
+
+Initially after this I became blank because no approach came into my mind for the final answer.
+
+Then later I understood that B position is basically:
+
+normal moves + collisions
+
+Because:
+
+even if collision does not happen, B still moves +1 every hour
+ so normal movement is always:
+
+k - 1
+
+
+Then whenever collision happens, B gets shifted one extra +1.
+
+So total movement of B becomes:
+(k - 1) + collisions
+
+Then I got confused at the end while finding the actual position of B after all iterations.
+
+For even n:
+
+ collision never happens
+ so B and A just move normally every hour
+
+So answer is basically movement after k - 1 moves.
+
+Then I understood that movement can exceed the n range because positions are circular.
+
+So % n wraps it back to the actual position in the position array.
+
+Same for odd n:
+
+ B normally moves +1 every hour
+ except when collision happens, then B moves one extra +1
+
+So for odd n:
+
+ first get interval when collision occurs
+ then calculate under k - 1 hours how many collisions can happen
+ all these collisions affect B position
+ so B gets:
+
++ total_collisions
+
+extra movement.
+
+Then normal movement which happens regardless of collision is:
+
+k - 1
+
+So total movement becomes:
+
+(k - 1) + total_collisions
+
+Then this can exceed the n range, so % n wraps it.
+
+Then I understood why +1 is needed.
+
+Because % n gives values in:
+
+0 to n - 1
+
+which is basically 0-based indexing.
+
+But problem positions are:
+
+1 to n
+
+so final answer becomes:
+((k - 1 + collisions) % n) + 1
+
+I think my overall logic building and formulas were mostly correct.
+I was mainly getting confused at the ending part while translating total movement into the actual final position of B.
+*/
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n % 2 == 0)
+    {
+        cout << ((k - 1) % n) + 1 << endl;
+    }
+    else
+    {
+        cout << (((k - 1) + ((k - 1) / ((n - 1) / 2))) % n) + 1 << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
+
+/*
+⣿⣿⣿⣿⣿⣷⣿⣿⣿⡅⡹⢿⠆⠙⠋⠉⠻⠿⣿⣿⣿⣿⣿⣿⣮⠻⣦⡙⢷⡑⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌⠡⠌⠂⣙⠻⣛⠻⠷⠐⠈⠛⢱⣮⣷⣽⣿
+⣿⣿⣿⣿⡇⢿⢹⣿⣶⠐⠁⠀⣀⣠⣤⠄⠀⠀⠈⠙⠻⣿⣿⣿⣦⣵⣌⠻⣷⢝⠦⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣻⣿⣊⡃⠀⣙⠿⣿⣿⣿⣎⢮⡀⢮⣽⣿⣿
+⢿⣿⣿⣿⣧⡸⡎⡛⡩⠖⠀⣴⣿⣿⣿⠀⠀⠀⠀⠸⠇⠀⠙⢿⣿⣿⣿⣷⣌⢷⣑⢷⣄⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣫⠶⠛⠉⠀⠁⠀⠈⠈⠀⠠⠜⠻⣿⣆⢿⣼⣿⣿⣿
+⢐⣿⣿⣿⣿⣧⢧⣧⢻⣦⢀⣹⣿⣿⣿⣇⠀⠄⠀⠀⠀⡀⠀⠈⢻⣿⣿⣿⣿⣷⣝⢦⡹⠷⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿⠈⠁⠀⠀⠀⠁⠀⠀⠀⠱⣶⣄⡀⠀⠈⠛⠜⣿⣿⣿⣿
+⠀⠊⢫⣿⣏⣿⡌⣼⣄⢫⡌⣿⣿⣿⣿⣿⣦⡈⠲⣄⣤⣤⡡⢀⣠⣿⣿⣿⣿⣿⣿⣷⣼⣍⢬⣦⡙⣿⣿⣿⣿⣿⣯⢁⡄⠀⡀⡀⠀⠄⢈⣠⢪⠀⣿⣿⣿⣦⠀⢉⢂⠹⡿⣿⣿
+⠀⠀⠄⢹⢃⢻⣟⠙⣿⣦⠱⢻⣿⣿⣿⣿⣿⣿⣷⣬⣍⣭⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡙⢿⣼⡿⣿⣿⣿⣿⣿⣷⣄⠘⣱⢦⣤⡴⡿⢈⣼⣿⣿⣿⣇⣴⣶⣮⣅⢻⣿⡏
+⠀⠀⠈⠹⣇⢡⢿⡆⠻⣿⣷⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣍⡻⣿⣟⣻⣿⣿⣿⣿⣷⣦⣥⣬⣤⣴⣾⣿⣿⣿⣿⣷⣿⣿⣿⣿⣷⡜⠃
+⠀⠀⠀⢀⣘⠈⢂⠃⣧⡹⣿⣷⡄⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣅⡙⢿⣟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⡕⠂
+⠀⠀⠀⠀⠀⠀⠛⢷⣜⢷⡌⠻⣿⣿⣦⣝⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣹⣷⣦⣹⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠉⠃⠀
+*/
